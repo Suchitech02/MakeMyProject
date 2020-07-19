383847461bf43949e343cbdce087c0f1f133f877
Suchi Chaudhary <Suchi.Chaudhary@Perpello.se>
2020-07-19 18:25:38 +0200

    Views-Route

--- MakeMyProject/App_Start/RouteConfig.cs
+++ MakeMyProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MakeMyProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PageData", action = "PageData", id = UrlParameter.Optional }
             );
         }
     }
